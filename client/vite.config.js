b1e5742e2b5a2819dbab34e5fbad1fb50cfb2b56
tarunsinghofficial <tarunsinghwap7@gmail.com>
2026-01-29 19:45:15 +0530

    fix: allow all request from any host

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,4 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  preview: {
+    host: true, // Allow all hosts (for production deployment)
+    port: 3000,
+  },
 });
